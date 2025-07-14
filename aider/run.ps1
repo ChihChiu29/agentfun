112c4576ada987cd70542ae4204f561b5817902b
chih chiu <chih.chiu.19@gmail.com>
2025-07-14 01:26:29 -0700

    more

--- aider/run.ps1
+++ aider/run.ps1
@@ -2,5 +2,6 @@
 # set OLLAMA_API_BASE="http://192.168.1.71:11434"
 
 $Env:OLLAMA_API_BASE = "http://192.168.1.71:11434"
-~\.local\bin\aider --model ollama_chat/qwen3:8b
+# ~\.local\bin\aider --model ollama_chat/qwen3:8b
 # ~\.local\bin\aider --model ollama_chat/gemma3n
+~\.local\bin\aider --model ollama_chat/gemma3:12b
