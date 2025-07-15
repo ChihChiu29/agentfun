e76393b0bb502b4067d6cc3bf4001394ae2596d0
chih chiu <chih.chiu.19@gmail.com>
2025-07-14 20:42:44 -0700

    add powershell script

--- aider/run.ps1
+++ aider/run.ps1
@@ -4,4 +4,5 @@
 $Env:OLLAMA_API_BASE = "http://192.168.1.71:11434"
 # ~\.local\bin\aider --model ollama_chat/qwen3:8b
 # ~\.local\bin\aider --model ollama_chat/gemma3n
-~\.local\bin\aider --model ollama_chat/gemma3:12b
+# ~\.local\bin\aider --model ollama_chat/gemma3:12b
+~\.local\bin\aider --model ollama_chat/deepseek-coder:6.7b
